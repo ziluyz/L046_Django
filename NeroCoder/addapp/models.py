b73033603adb881f872cdcb28e1da3efa5c367d5
Ilya <ziluyz@gmail.com>
2024-08-02 07:04:47 +0300

    added Author

--- NeroCoder/addapp/models.py
+++ NeroCoder/addapp/models.py
@@ -5,6 +5,7 @@ class News_post(models.Model):
 	short_description = models.CharField('Краткое описание новости', max_length=200)
 	text = models.TextField('Новость')
 	pub_date = models.DateTimeField('Дата публикации')
+	author = models.CharField('Автор', max_length=50)
 	def __str__(self):
 		return self.title
 	class Meta:
