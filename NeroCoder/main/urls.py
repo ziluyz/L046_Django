8cbfbe92cf75d7a25c4f4edd59d0c1000cee47bb
Ilya <ziluyz@gmail.com>
2024-08-02 05:42:49 +0300

    second lesson

--- NeroCoder/main/urls.py
+++ NeroCoder/main/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new)
+    path('', views.index, name='home'),
+    path('new', views.new, name='page2'),
+    path('plankton', views.plankton, name='plankton'),
+    path('crabs', views.crabs, name='crabs'),
+    path('octopus', views.octopus, name='octopus'),
+    path('bivalves', views.bivalves, name='bivalves'),
 ]
--- NeroCoder/main/views.py
+++ NeroCoder/main/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'main/index.html', {'title': 'Главная страница DogDjango', "active": 1})
 
 def new(request):
-    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+    return render(request, 'main/new.html', {'title': 'Новые открытия в области океанской флоры', "active": 2})
+
+def plankton(request):
+    return render(request, 'main/plankton.html', {'title': 'Польза планктона', "active": 3})
+
+def crabs(request):
+    return render(request, 'main/crabs.html', {'title': 'Океанские крабы', "active": 4})
+
+def octopus(request):
+    return render(request, 'main/octopus.html', {'title': 'Осьминоги', "active": 5})
+
+def bivalves(request):
+    return render(request, 'main/bivalves.html', {'title': 'Океанские двустворчатые моллюски', "active": 6})
